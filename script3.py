f9c9198acc290ffd3fe44d83b782a70c5bacb3f3
narender5162 <150369837+narender5162@users.noreply.github.com>
2023-11-09 17:50:40 +0000

    Add files via upload

--- script3.py
+++ script3.py
@@ -1,25 +1,45 @@
-# Importing the required Libraries
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Data for population (stored in a DataFrame)
+# Data for different countries (stored in a DataFrame)
 data = {
-    'Gender': ['Males', 'Females'],
-    'Population (millions)': [623.7, 586.5]
+    'Country': [
+        'China', 'India', 'Indonesia', 'Korea, Rep.', 'Saudi Arabia',
+        'Turkiye', 'United Kingdom', 'United States',
+        'Spain', 'France', 'Germany',
+        'Italy', 'Japan', 'Russian Federation'
+    ],
+    'Population (millions)': [
+        1371.9, 1307.2, 256.2, 50.7, 32.1, 78.1,
+        64.6, 318.4, 46.7, 66.0, 80.9, 60.8, 127.1, 143.5
+    ],
+    'GDP (billions)': [
+        10476, 2039, 891, 1484, 767, 939,
+        3065, 17551, 1200, 2846, 3845, 2149, 4850, 2063
+    ]
 }
-# Creating a Pandas Data-Frame
+
 df = pd.DataFrame(data)
 
-# Create a pie chart
-labels = df['Gender']
-sizes = df['Population (millions)']
-colors = ['blue', 'pink']
+# Colors for each country
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'lime', 'purple',
+          'orange', 'pink', 'gold', 'lightblue', 'brown']
 
-# Setting Parameters
-plt.figure(figsize=(8, 8))
-plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
-plt.title('Population Distribution by Gender in India (2011, Excluding Transgender)', fontsize=14)
+# Create a scatter plot with legends and colors
+plt.figure(figsize=(10, 6))
 
-plt.axis('equal')  # Equal aspect ratio ensures that the pie is drawn as a circle.
+for i, row in df.iterrows():
+    country = row['Country']
+    population = row['Population (millions)']
+    gdp = row['GDP (billions)']
+    plt.scatter(population, gdp, marker='o', s=100, color=colors[i], label=country)
+
+plt.title('Scatter Plot of GDP vs. Population (2014)')
+plt.xlabel('Population (millions)')
+plt.ylabel('GDP (billions)')
+plt.grid(True)
+
+# Customize the legend by specifying the number of columns
+plt.legend(loc='upper left', ncol=1, fontsize=8)
 
 plt.show()  # Showing the Final Results.
